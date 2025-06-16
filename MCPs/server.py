eb76639e37ba8d7d28bd3467208f5a850f504d91
moguism <mauriciorosso.aboy@gmail.com>
2025-06-16 11:35:53 +0200

    MCP listo :D
    Funciona de maravilla en n8n

--- MCPs/server.py
+++ MCPs/server.py
@@ -1,18 +1,24 @@
-from mcp.server.fastmcp import FastMCP
 import requests
 import constants
+from fastapi import FastAPI
+from fastapi_mcp import FastApiMCP
+import uvicorn
 
-# Crea el servidor MCP
-mcp = FastMCP("MCP")
+# El servidor base es FastAPI
+app = FastAPI(debug=True, title="MCP API")
 
 # Obtiene todos junto con sus relaciones del servidor de Laravel
-@mcp.tool()
+@app.get("/hotels-services")
 def obtain_all_hotels_services() -> int:
     response = requests.get(str(constants.API_URL, "hotels-services/all"))
     json = response.json()
     print(json)
     return json
 
-# Inicia el servidor (no viene en el ejemplo oficial por alguna razón). Usa el puerto 5000
+mcp = FastApiMCP(app)
+mcp.mount() # Para tener acceso al MCP
+
+# Inicia el servidor
 if __name__ == "__main__":
-    mcp.run("sse")
+    # "server" es el nombre del archivo, y "app" es el de la variable que he creado antes
+    uvicorn.run("server:app", port=5000, reload=True)
